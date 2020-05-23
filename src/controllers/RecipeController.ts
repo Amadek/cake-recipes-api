a4cbcacfb81b75e54224d5e8f8b9280f3bd7ec87
Michal Janus <michal@yvnash.xyz>
2020-05-23 23:45:24 +0200

    Added database query methods without tests

--- src/controllers/RecipeController.ts
+++ src/controllers/RecipeController.ts
@@ -2,8 +2,13 @@ import express, { Router, Request, Response, NextFunction } from 'express';
 import { Recipe } from '../entities/Recipe';
 import { BadRequest } from 'http-errors';
 import { Db } from 'mongodb';
+<<<<<<< HEAD
 import { ITokenValidator } from './ITokenValidator';
 import { IRecipeParser } from './IRecipeParser';
+=======
+import { RecipeParser } from './RecipeParser';
+import { validator } from './validator';
+>>>>>>> Added database query methods without tests
 
 /**
  * Controller performing CRUD operations on recipes.
@@ -39,6 +44,47 @@ export class RecipeController {
       .catch(next);
   }
 
+  /**
+   * Gets recipe from DB
+   * @param req request containing recipe name  
+   * @param res response
+   * @param next express callback
+   */
+  public getRecipe (req: Request, res: Response, next: NextFunction, recipeName:string): void {
+    var sanitizer = require('Sanitize')();
+    Promise.resolve()
+    .then(() => sanitizer.Sanitize(recipeName))
+    .then(()=> {
+      if (!validator.isAlphanumeric(recipeName)) throw new BadRequest();
+
+      return this._fetchRecipeFromDbByName(recipeName);
+      })
+
+    .then(() => res.status(200).send('OK'))
+    .catch(next)
+  }
+
+
+  /**
+   * Gets multiple recipes from database 
+   * @param req request containing letters 
+   * @param res response
+   * @param next express callback
+   */
+  public getMultipleRecipes (req: Request, res: Response, next: NextFunction, query:string): void {
+    var sanitizer = require('Sanitize')();
+    Promise.resolve()
+    .then(() => sanitizer.Sanitize(query))
+    .then(()=> {
+      if (!validator.isAlphanumeric(query)&& query.length < 3 ) throw new BadRequest();
+
+      return this._fetchMultipleRecipes(query);
+      })
+
+    .then(() => res.status(200).send('OK'))
+    .catch(next)
+  }
+
   private _parseRecipe (recipeJson: any): Recipe | null {
     return this._recipeParser.parse(recipeJson);
   }
@@ -48,6 +94,7 @@ export class RecipeController {
 
     return this._db.collection('recipe').insertOne(recipe);
   }
+<<<<<<< HEAD
 
   private _validateToken (req: Request): Promise<void> {
     if (!req.headers.authorization) throw new BadRequest();
@@ -61,3 +108,15 @@ export class RecipeController {
       });
   }
 }
+=======
+  
+  private _fetchRecipeFromDbByName (name: string): Promise<any> {
+    return this._db.collection('recipe').findOne({name});
+  }
+  
+  private _fetchMultipleRecipes (query: string): Promise<any> {
+    return this._db.collection('recipe').find({Name: new RegExp('^' + query)}).toArray();
+  }
+}
+
+>>>>>>> Added database query methods without tests
