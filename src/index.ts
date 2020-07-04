8646244c99e3b2f68bea362f6628634717ce4597
Amadeusz Pecherczyk <a.pecherczyk@protonmail.com>
2020-07-04 20:44:06 +0200

    next callback in cors middleware

--- src/index.ts
+++ src/index.ts
@@ -20,10 +20,11 @@ Promise.resolve()
   .then(() => new DbConnector(config).connect())
   .then(db => new DbInitializer(db).initialize())
   .then(db => {
-    app.use((_req, res) => {
+    app.use((_req, res, next) => {
       res.header('Access-Control-Allow-Origin', '*');
       res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
       res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+      next();
     });
     app.use(express.json());
     app.use('/auth', new AuthController(axios, jwtManager, config).route());
